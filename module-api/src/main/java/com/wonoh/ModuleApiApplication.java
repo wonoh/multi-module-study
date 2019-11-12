3efe92baaafd39bcec868e2f8a452a3768e8dba2
지원오 <wonoh@zuminternet.com>
2019-11-12 13:42:16 +0900

    api module 등록

---
+++ module-api/src/main/java/com/wonoh/ModuleApiApplication.java
@@ -0,0 +1,11 @@
+package com.wonoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiApplication.class,args);
+    }
+}
